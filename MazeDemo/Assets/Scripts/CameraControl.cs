be74a60a5e7fdf3c349a6d52e3fd85bcf398de19
Nick Pang <n17pang@gmail.com>
2024-01-30 16:24:42 -0500

    Pushed old changes that haven't been pushed for some reason.

--- MazeDemo/Assets/Scripts/CameraControl.cs
+++ MazeDemo/Assets/Scripts/CameraControl.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    // Control how fast the camera moves relative to mouse movement. Higher = faster
     public float mouseSensitivity = 300f;
 
+    // This is defined in the Unity development interface
     public Transform playerBody;
 
+    // Represents vertical rotation
     float xRotation = 0f;
     
     // Start is called before the first frame update
@@ -19,13 +22,18 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Inverting the vertical movement of the mouse to make moving it up correlate with up
         xRotation -= mouseY;
+        // Restrict the player's ability to look up and down
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        // Rotates the camera vertically
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // Rotates the camera horizontally
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
